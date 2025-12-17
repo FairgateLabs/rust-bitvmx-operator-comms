95d78f626c1e87a2bf646862ec0bf5d67909ed71
Martin Jonas <martinjonas@gmail.com>
2025-12-17 20:58:26 -0300

    completely remove features

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,1 @@
-// --- Feature conflict detection ---
-#[cfg(all(feature = "memory-impl", feature = "broker-impl"))]
-compile_error!("You cannot enable both `memory-impl` and `broker-impl` features at the same time.");
-
-// --- No backend selected ---
-#[cfg(not(any(feature = "memory-impl", feature = "broker-impl")))]
-compile_error!("You must enable either the `memory-impl` or `broker-impl` feature.");
-
-// --- Conditional exports ---
-#[cfg(feature = "memory-impl")]
-pub use ::memory::*;
-
-#[cfg(feature = "broker-impl")]
 pub use ::broker::*;
